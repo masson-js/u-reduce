94fae391b34b3bee6b38e35767bc7595cbbfba78
Massonn.js <68811288+masson-js@users.noreply.github.com>
2022-08-31 21:02:42 +0200

    31-1

--- src/Pages/CodeExamples.js
+++ src/Pages/CodeExamples.js
@@ -1,28 +1,10 @@
 import React from 'react';
+import data from '../StorageExamples/DataExamples'
 
 import './Examples.css';
 
 function CodeExamples() {
 
-  const data = [
-    {
-      nameComponent:'CodeExamples',
-      id: 1,
-      headerText : 'Example Header text',
-      diffecaltLevel : 'easy',
-      codeExample : 'code',
-      mainText : 'This is main description text',
-    },
-    {
-      nameComponent:'CodeExamples',
-      id: 2,
-      headerText : 'Example Header text',
-      diffecaltLevel : 'hard',
-      codeExample : 'code',
-      mainText : 'This is main description text',
-    }
-  ]
-  
   return (
     <>
       {data.map((example) => (
@@ -43,53 +25,6 @@ function CodeExamples() {
         </div>
         ))
       }
-
-
-
-
-
-
-    {/* <div className="CodeExamplesComponent" id='1'>
-      <header className="ExampleHeader">
-        <h2 className="ExampleId">id:1</h2>
-        <h2 className="ExampleHeaderText">Example Header text</h2>
-        <h2 className="DiffecaltLevelEasy">Diffecalt level</h2>
-      </header>
-      <div className="ExampleContent">
-        <p> Paragraph text Up</p>
-        <pre className='codeText'>
-          
-          <span class="token comment">// <code>Arrow()</code> function</span><br></br>
-          <span class="token function">reduce</span>
-          
-
-        </pre>
-        <p>Lorem Ipsum is simply dummy text of the printing and 
-          typesetting industry. Lorem Ipsum has been the industry's 
-          standard dummy text ever since the 1500s, when an unknown </p>
-      </div>
-      
-    </div>
-        <div className="CodeExamplesComponent"  id='2'>
-        <header className="ExampleHeader">
-          <h2 className="ExampleId">id:2</h2>
-          <h2 className="ExampleHeaderText">Example Header text</h2>
-          <h2 className="DiffecaltLevelHard">Diffecalt level</h2>
-        </header>
-        <div className="ExampleContent">
-          <p> Paragraph text Up</p>
-          <pre className='codeText'>
-            
-            <span class="token comment">// <code>Arrow()</code> function</span><br></br>
-            <span class="token function">reduce</span>
-            
-  
-          </pre>
-          <p>Lorem Ipsum is simply dummy text of the printing and 
-            typesetting industry. Lorem Ipsum has been the industry's 
-            standard dummy text ever since the 1500s, when an unknown </p>
-        </div>
-      </div> */}
     </>
     
     );
---
+++ src/StorageExamples/DataExamples.js
@@ -0,0 +1,20 @@
+const data = [
+  {
+    nameComponent:'CodeExamples',
+    id: 1,
+    headerText : 'Example Header text',
+    diffecaltLevel : 'easy',
+    codeExample : 'code',
+    mainText : 'This is main description text',
+  },
+  {
+    nameComponent:'CodeExamples',
+    id: 2,
+    headerText : 'Example Header text',
+    diffecaltLevel : 'hard',
+    codeExample : 'code',
+    mainText : 'This is main description text',
+  }
+]
+
+export default data 
