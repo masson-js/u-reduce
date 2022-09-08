78ea88bb288978183a9a1411e8249938fcfca14e
Wlod <68811288+masson-js@users.noreply.github.com>
2022-09-08 21:27:25 +0200

    08-01

---
+++ src/storage/list-of-videos.js
@@ -0,0 +1,10 @@
+const url = [
+  'https://youtu.be/tVCYa_bnITg',
+  'https://youtu.be/-LFjnY1PEDA',
+  'https://youtu.be/g1C40tDP0Bk' ,
+  'https://youtu.be/s1XVfm5mIuU',
+  'https://youtu.be/NiLUGy1Mh4U',
+  '',
+]
+
+export default url
